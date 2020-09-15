5c02479182df77a0af566ccad4ebc8c3e781b832
nuwan_r <nuwan_r@epiclanka.net>
2020-09-15 20:28:32 +0530

    add some configurations

--- auth-service/src/main/java/com/techiprimers/authorization/authservice/config/WebSecurityConfiguration.java
+++ auth-service/src/main/java/com/techiprimers/authorization/authservice/config/WebSecurityConfiguration.java
@@ -4,5 +4,79 @@
  **/
 package com.techiprimers.authorization.authservice.config;
 
-public class WebSecurityConfiguration {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@Order(SecurityProperties.BASIC_AUTH_ORDER)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Resource(name = "userDetailsService")
+    private final UserDetailsService userDetailsService;
+
+    private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
+
+    @Autowired
+    public WebSecurityConfiguration(UserDetailsService userDetailsService, CustomLogoutSuccessHandler customLogoutSuccessHandler) {
+        this.userDetailsService = userDetailsService;
+        this.customLogoutSuccessHandler = customLogoutSuccessHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/oauth/check_token","/oauth/token").permitAll().and().exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and().logout().logoutUrl("/oauth/logout").logoutSuccessHandler(customLogoutSuccessHandler)
+                .and().authorizeRequests().antMatchers("/**").authenticated().and()
+                .httpBasic();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization","content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Collections.singletonList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
